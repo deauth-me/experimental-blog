3f1b72bfe1852d0e09515abb363be2217781ed17
unknown <m@vse-03968.ad.vse.cz>
2023-04-14 15:36:19 +0200

    Fix dependency issues

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Blog</h1>
+      {/* Render your blog posts here */}
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Post.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Post() {
+  return (
+    <div>
+      <h1>Post Title</h1>
+      {/* Render your blog post content here */}
+    </div>
+  );
+}
+
+export default Post;
